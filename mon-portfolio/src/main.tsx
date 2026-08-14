74a703c47d16597fa169865de36f40c6987470c9
massodanielle5-code <massodanielle5@gmail.com>
2026-08-14 03:14:35 +0100

    ga4 cancelled

--- mon-portfolio/src/main.tsx
+++ mon-portfolio/src/main.tsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import ReactGA from 'react-ga4';
-
-ReactGA.initialize('G-EVY3613F93'); 
-ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
